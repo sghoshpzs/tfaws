2caf2ca0d54686f0fbb4deaddcfcefa4f0752884
Sourav Ghosh <sourav.ghosh1@bp.com>
2024-01-09 14:39:38 +0000

    -

--- geldsack/ec2.tf
+++ geldsack/ec2.tf
@@ -4,8 +4,8 @@ resource "aws_instance" "geldsack"{
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id = element(aws_subnet.geldsack_private_subnets[*].id, count.index)
-  security_groups = aws_security_group.geldsack_sg.id
+  security_groups = [aws_security_group.geldsack_sg.id]
   tags = {
-    Name = "${var.ec2_instance_name}-${count.index + 1}"
+    Name = length(var.private_sunbet_cidr) > 0 ? "${var.ec2_instance_name}-${count.index + 1}" : "${var.ec2_instance_name}"
   }
 }
