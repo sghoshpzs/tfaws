078151634007718060532c67b7b74c58efb96074
Sourav Ghosh <s.ghosh.pzs@gmail.com>
2025-04-24 03:15:04 +0000

    -

--- init.tf
+++ init.tf
@@ -13,11 +13,8 @@ provider "aws" {
 }
 
 
-module "geldsack" {
-  source              = "./geldsack"
-  ami                 = var.ami
-  instance_type       = var.instance_type
-  ec2_instance_name   = var.ec2_instance_name
+module "vpc" {
+  source              = "./modules/vpc"
   vpc_name            = var.vpc_name
   vpc_cidr            = var.vpc_cidr
   private_sunbet_cidr = var.private_sunbet_cidr
@@ -26,6 +23,16 @@ module "geldsack" {
   public_sunbet_name  = var.public_sunbet_name
   sg_egress_ports     = var.sg_egress_ports
   sg_ingress_ports    = var.sg_ingress_ports
+}
+
+
+module "ec2" {
+  source              = "./modules/ec2"
+  ami                 = var.ami
+  instance_type       = var.instance_type
+  ec2_instance_name   = var.ec2_instance_name
+  private_sunbet_cidr = var.private_sunbet_cidr
+  public_sunbet_cidr  = var.public_sunbet_cidr
   geldsack_ssh_key    = var.geldsack_ssh_key
 }
 
@@ -33,7 +40,6 @@ module "geldsack" {
 #   value = module.geldsack.vpc_id
 # }
 
-output "ec2_public_ip_address" {
-  
+output "ec2_public_ip_address" {  
   value = module.geldsack.ec2_public_ip_addresses
 }
