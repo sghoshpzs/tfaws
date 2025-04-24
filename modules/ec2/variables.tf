395ec4d9295418d4da155f690141fb42b3b4a055
Sourav Ghosh <83961020+sghoshpzs@users.noreply.github.com>
2025-04-24 08:52:56 +0530

    -

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -13,23 +13,8 @@ variable "ec2_instance_name"{
     nullable = false
 }
 
-variable "vpc_name"{
-    type = string
-    nullable = false
-}
-
-variable "vpc_cidr"{
-    type = string
-    nullable = false
-}
-
 variable "private_sunbet_cidr"{
     type = list(string)
-    nullable = false
-}
-
-variable "private_sunbet_name"{
-    type = string
     nullable = false
 }
 
@@ -38,30 +23,14 @@ variable "public_sunbet_cidr"{
     nullable = false
 }
 
-variable "public_sunbet_name"{
-    type = string
-    nullable = false
-}
-
 variable "public_subnet_az"{
     type = list(string)
     default = [ "ap-south-1a", "ap-south-1b", "ap-south-1c" ]
 }
 
-
 variable "private_subnet_az"{
     type = list(string)
     default = [ "ap-south-1a", "ap-south-1b", "ap-south-1c" ]
-}
-
-variable "sg_ingress_ports" {
-    type = list(list(string))
-    nullable = false
-}
-
-variable "sg_egress_ports" {
-    type = list(list(string))
-    nullable = false
 }
 
 variable "geldsack_ssh_key" {
